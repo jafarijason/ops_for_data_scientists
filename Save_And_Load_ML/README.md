7aaf6fa0df8eabbdb8f6884c39fa56158a8e38a0
Jason Jafari <me@jasonjafari.com>
2022-03-21 02:33:57 -0400

    OK

---
+++ Save_And_Load_ML/README.md
@@ -0,0 +1,27 @@
+# [Save and Load Machine Learning Models in Python with scikit-learn](https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/)
+
+## Installing pip for Python 3
+```
+sudo apt update
+sudo apt install python3-pip
+```
+
+## Installing virtualenv
+```
+python3 -m pip install --user virtualenv
+sudo pip3 install virtualenv
+```
+
+```
+cd Save_And_Load_ML
+virtualenv env
+source ./venv/bin/activate
+pip install pandas sklearn
+
+pip freeze  >  requirements.txt
+
+```
+
+```
+python save_model.py
+```
---
+++ Save_And_Load_ML/save_model.py
@@ -0,0 +1,42 @@
+import pandas
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+import pickle
+url = "pima-indians-diabetes.data.csv"
+names = [
+    'preg',
+    'plas',
+    'pres',
+    'skin',
+    'test',
+    'mass',
+    'pedi',
+    'age',
+    'class'
+]
+dataframe = pandas.read_csv(url, names=names)
+array = dataframe.values
+X = array[:, 0:8]
+Y = array[:, 8]
+test_size = 0.33
+seed = 7
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(
+    X,
+    Y,
+    test_size=test_size,
+    random_state=seed
+)
+# Fit the model on training set
+model = LogisticRegression()
+model.fit(X_train, Y_train)
+# save the model to disk
+filename = 'finalized_model.sav'
+pickle.dump(model, open(filename, 'wb'))
+
+
+# load the model from disk
+loaded_model = pickle.load(open(filename, 'rb'))
+result = loaded_model.score(X_test, Y_test)
+predict = loaded_model.predict([X_test[8]])
+print(result)
+print(predict)
