12d461eda3e34239fb64ab7295276b0bb889ae8e
Jason Jafari <me@jasonjafari.com>
2022-03-22 02:24:47 -0400

    OK

--- Save_And_Load_ML/main.py
+++ Save_And_Load_ML/main.py
@@ -1,36 +1,9 @@
+import pickle
 from fastapi import FastAPI, Request
 import pandas
 from sklearn import model_selection
 from sklearn.linear_model import LogisticRegression
-
-
-url = "pima-indians-diabetes.data.csv"
-names = [
-    'preg',
-    'plas',
-    'pres',
-    'skin',
-    'test',
-    'mass',
-    'pedi',
-    'age',
-    'class'
-]
-dataframe = pandas.read_csv(url, names=names)
-array = dataframe.values
-X = array[:, 0:8]
-Y = array[:, 8]
-test_size = 0.33
-seed = 7
-X_train, X_test, Y_train, Y_test = model_selection.train_test_split(
-    X,
-    Y,
-    test_size=test_size,
-    random_state=seed
-)
-
-
-import pickle
+import json
 
 
 filename = 'finalized_model.sav'
@@ -48,8 +21,5 @@ async def root():
 @app.post("/loadAndServe")
 async def loadAndServe(request: Request):
     body = await request.json()
-    # result = loaded_model.score(X_test, Y_test)
-    # result = loaded_model.predict([[ 13.   , 153.   ,  88.   ,  37.   , 140.   ,  40.6  ,   1.174,
-    # #     39.   ]])
-    # print(loaded_model)
-    return body
+    predict = loaded_model.predict(body).tolist()
+    return predict
