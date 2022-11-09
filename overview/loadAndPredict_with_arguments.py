f3e9a2131d900b65aff832e3f65847aad805d524
Jason Jafari <me@jasonjafari.com>
2022-11-09 17:57:19 -0500

    OK

---
+++ overview/loadAndPredict_with_arguments.py
@@ -0,0 +1,14 @@
+from sklearn.ensemble import IsolationForest
+import pickle 
+import sys
+
+myInput=[[int(sys.argv[1])], [int(sys.argv[2])], [int(sys.argv[3])]]
+
+filename = 'myModel.pkl'
+loaded_model = pickle.load(open(filename, 'rb'))
+
+
+anomalyScore = loaded_model.score_samples(myInput)
+
+print(anomalyScore)
+
